19ba0937609d41c78dd4078f5ef571f868606ee7
Harrison Tran <harrisontran@Harrisons-MacBook-Pro.local>
2022-06-06 21:16:04 -0700

    second commit

---
+++ CodeWithMosh/Ultimate React Native- Part 1/Section 4- Styles/End/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { View } from "react-native";
+
+import ViewImageScreen from "./app/screens/ViewImageScreen";
+
+export default function App() {
+  return <ViewImageScreen />;
+}
---
+++ CodeWithMosh/Ultimate React Native- Part 1/Section 5- Lists/End/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Screen from "./app/components/Screen";
+import Icon from "./app/components/Icon";
+import ListItem from "./app/components/ListItem";
+import AccountScreen from "./app/screens/AccountScreen";
+import ListingsScreen from "./app/screens/ListingsScreen";
+
+export default function App() {
+  return <AccountScreen />;
+}
